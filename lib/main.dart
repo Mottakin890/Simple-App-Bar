a23cf61d59c86b26603588fb0f9d47c05f17cfe2
Mottakinul Alam <mottakinulalam@gmail.com>
2024-05-23 21:53:55 +0100

    App Bar Added

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(app());
+}
+class app extends StatelessWidget{
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
+
+class MainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text("A P P B A R"),
+        centerTitle: true,
+        elevation: 3,
+        bottomOpacity: 10,
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.dark_mode_outlined))
+        ],
+        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.person_outline_rounded)),
+      ),
+    );
+  }
+}
